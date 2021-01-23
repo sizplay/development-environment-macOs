84f9a2c53b2e58595239b1a101be6f0a9ea2f690
Chaehoon Lim <sizplay@gmail.com>
2021-01-23 16:17:33 +0900

    Update install.sh

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -3,3 +3,5 @@
 cp ./iterm2/com.googlecode.iterm2.plist ~/Library/Application\ Support/iTerm2/DynamicProfiles/
 
 chsh -s /bin/zsh
+
+# double click Snazzy.itermcolors and goto profile colors load presets
